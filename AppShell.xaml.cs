0fc7a0e234fe15b64259df567e3dd3ae0dd9df9e
Emimint <90863470+Emimint@users.noreply.github.com>
2024-02-19 08:16:06 -0500

    remove main since not used

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -9,7 +9,6 @@ namespace SimulateurATM
             InitializeComponent();
             //Register all routes
             Routing.RegisterRoute("login", typeof(LoginPage));
-            Routing.RegisterRoute("main", typeof(MainPage));
             Routing.RegisterRoute("home", typeof(HomePage));
             Routing.RegisterRoute("logout", typeof(SettingsPage));
             Routing.RegisterRoute("account", typeof(AccountPage));
