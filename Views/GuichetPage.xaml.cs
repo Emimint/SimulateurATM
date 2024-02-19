0899aadd55a90fd16d96725c32c4a2746dd53eb9
Emimint <90863470+Emimint@users.noreply.github.com>
2024-02-19 14:17:26 -0500

    start implementing logic in guichet page

--- Views/GuichetPage.xaml.cs
+++ Views/GuichetPage.xaml.cs
@@ -1,9 +1,31 @@
-namespace SimulateurATM.Views;
+using Microsoft.Maui.Controls;
+using System.Windows.Input;
 
-public partial class GuichetPage : ContentPage
+namespace SimulateurATM.Views
 {
-	public GuichetPage()
-	{
-		InitializeComponent();
-	}
+    public partial class GuichetPage : ContentPage
+    {
+        public ICommand AddCharCommand { get; private set; }
+
+        public GuichetPage()
+        {
+            InitializeComponent();
+           // BindingContext = new GuichetPage();
+            AddCharCommand = new Command<string>(ExecuteAddCharCommand);
+        }
+
+        private void ExecuteAddCharCommand(string param)
+        {
+            textInput.Text += param;
+        }
+
+        private async void LogoutButton_Clicked(object sender, EventArgs e)
+        {
+            if (await DisplayAlert("Confirmation", "Voulez vous quitter votre session?", "Oui", "Non"))
+            {
+                SecureStorage.RemoveAll();
+                await Shell.Current.GoToAsync("///login");
+            }
+        }
+    }
 }
