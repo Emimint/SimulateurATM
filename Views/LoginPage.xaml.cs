f34db750f8715b1579ce8f67ff9317c7fb8f6c3d
Emimint <90863470+Emimint@users.noreply.github.com>
2024-02-19 09:05:34 -0500

    update login logic

--- Views/LoginPage.xaml.cs
+++ Views/LoginPage.xaml.cs
@@ -2,6 +2,7 @@ namespace SimulateurATM.Views;
 
 public partial class LoginPage : ContentPage
 {
+    int nbrTentatives = 2;
 	public LoginPage()
 	{
 		InitializeComponent();
@@ -22,7 +23,13 @@ public partial class LoginPage : ContentPage
         }
         else
         {
-            await DisplayAlert("Echec de la connexion", "Le nom d'utilisateur ou mot de passe sont incorrects", "Essayez a nouveau");
+            if(nbrTentatives >= 1)
+                await DisplayAlert("Echec de la connexion", $"Le nom d'utilisateur ou mot de passe sont incorrects. Essayez à nouveau (encore {nbrTentatives} tentative{(nbrTentatives-- == 1 ? "" : "s")}).", "OK");
+            else
+            { 
+                await DisplayAlert("Echec de la connexion", "Vous avez atteint le maximum de tentatives autorisees. Veuillez revenir plus tard.", "OK");
+                LoginButton.IsEnabled = false;
+            }
         }
     }
 
