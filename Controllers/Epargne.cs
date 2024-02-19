f8652fc5cfb85bba452c6c4a4ca5f0270b28c6a9
Emimint <90863470+Emimint@users.noreply.github.com>
2024-02-19 08:20:50 -0500

    add controller classes

---
+++ Controllers/Epargne.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulateurATM.Controllers
+{
+    public class Epargne : Compte
+    {
+        private float tauxInteret;
+
+        //        public Epargne() { }
+
+        //        public Epargne(string numeroNIP) { }
+
+        //        public void paiementInterets()
+        //        {
+
+        //        }
+    }
+}
---
+++ Controllers/Guichet.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulateurATM.Controllers
+{
+    public class Guichet
+    {
+
+        //        List<Client> clients;
+        //        List<Cheque> comptesCheque;
+        //        List<Epargne> comptesEpargne;
+
+        //        public static bool ValiderUtilisateur(string nom, string nip)
+        //        {
+        //            return false; // retourne boolean
+        //        }
+
+        //        public static float RetraitCheque(string nip, float montant)
+        //        {
+        //            return 0f;  //retourne le solde du compte
+        //        }
+
+        //        public static float RetraitEpargne(string nip, float montant)
+        //        {
+        //            return 0f;  //retourne le solde du compte
+        //        }
+
+        //        public static float DepotCheque(string nip, float montant)
+        //        {
+        //            return 0f; //retourne le solde du compte
+
+        //        }
+
+        //        public static float DepotEpargne(string nip, float montant)
+        //        {
+        //            return 0f;
+        //            // retourne le solde du compte
+        //        }
+
+    }
+}
